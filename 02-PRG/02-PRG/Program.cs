23fe24ba09a79b4cac85181851448178e1e93184
DrankerCZ <drankercz@gmail.com>
2023-09-21 23:49:18 +0200

    added a comment

--- 02-PRG/02-PRG/Program.cs
+++ 02-PRG/02-PRG/Program.cs
@@ -6,6 +6,8 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
+            // ...
+
             CalculatorContext context = new CalculatorContext(new OperationAdd());
 
             Console.WriteLine($"4 + 5 = {context.executeOperation(4,5)}");
