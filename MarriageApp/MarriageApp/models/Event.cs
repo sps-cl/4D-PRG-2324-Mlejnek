ad64466e252e21318c200c6a6a488d245890e3ab
drankercz <drankercz@gmail.com>
2023-10-02 15:13:07 +0200

    Edited Events

---
+++ MarriageApp/MarriageApp/models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarriageApp.models
+{
+    internal abstract class Event<T>
+    {
+        public int Order { get; set; }
+        public string Name { get; }
+
+        public Event(int order, string name)
+        {
+            Order = order;
+            Name = name;
+        }
+
+        public abstract void DisplayInfo();
+    }
+}
